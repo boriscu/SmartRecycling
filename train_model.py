199cc2efc5058b193fd53d4a5410949b98d83b61
Boris <borisculjak@gmail.com>
2023-10-06 12:50:27 +0200

    Implemented decaying learning rate

--- train_model.py
+++ train_model.py
@@ -19,9 +19,13 @@ y_train = y_train.flatten()
 y_test = y_test.flatten()
 
 # Label names
-label_names = ["Cardboard", "Glass bottle", "Can", "Crushed Can", "Plastic bottle"]
+label_names = ["Cardboard", "Glass bottle", "Can", "Plastic bottle"]
 
-# Plot a random image with label
+y_train[y_train == 3] = 2
+y_test[y_test == 3] = 2
+y_train[y_train == 4] = 3
+y_test[y_test == 4] = 3
+
 random_idx = np.random.randint(0, len(x_train))
 plt.imshow(x_train[random_idx])
 plt.title(f"Label: {label_names[y_train[random_idx]]}")
@@ -36,32 +40,37 @@ base_model_resnet = tf.keras.applications.ResNet50(
 # Freeze the base model layers
 base_model_resnet.trainable = False
 
-# Create a new model with custom classification layers
 model_resnet = models.Sequential(
     [
         base_model_resnet,
         layers.Flatten(),
         layers.Dense(512, activation="relu"),
         layers.Dropout(0.5),
-        layers.Dense(
-            5, activation="softmax"
-        ),  # There are 5 unique labels in the dataset
+        layers.Dense(4, activation="softmax"),
     ]
 )
 
+lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate=0.001,  # Starting learning rate
+    decay_steps=1000,  # Decay the learning rate after every 1000 steps
+    decay_rate=0.9,  # Decay rate
+)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
+
 model_resnet.compile(
-    optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+    optimizer=optimizer, loss="sparse_categorical_crossentropy", metrics=["accuracy"]
 )
 
 history = model_resnet.fit(
     x_train, y_train, epochs=60, validation_data=(x_test, y_test)
 )
 
-model_resnet.save("recycle_model.h5")
-
 loss_resnet, accuracy_resnet = model_resnet.evaluate(x_test, y_test)
 print(f"ResNet50 - Test Loss: {loss_resnet:.4f}")
 print(f"ResNet50 - Test Accuracy: {accuracy_resnet:.4f}")
+
+model_resnet.save("recycle_model.h5")
 
 # Plot training & validation accuracy values
 plt.figure(figsize=(12, 5))
@@ -86,32 +95,38 @@ plt.legend(["Train", "Test"], loc="upper left")
 plt.tight_layout()
 plt.show()
 
-# Randomly select an image from the test set
+# Predict the label for the selected image
 random_idx = np.random.randint(0, len(x_test))
 image = x_test[random_idx]
 true_label = y_test[random_idx]
 
-# Predict the label for the selected image
-# Expand dimensions for the model's expected input shape
 image_for_prediction = np.expand_dims(image, axis=0)
 predicted_scores = model_resnet.predict(image_for_prediction)
 predicted_label = np.argmax(predicted_scores)
 
-# Plot the image with true and predicted labels
 plt.imshow(image)
-plt.title(f"True Label: {true_label}\nPredicted Label: {predicted_label}")
+plt.title(
+    f"True Label: {label_names[true_label]}\nPredicted Label: {label_names[predicted_label]}"
+)
 plt.axis("off")
 plt.show()
 
+
+# Plot confusion matrix
 y_pred = model_resnet.predict(x_test)
 y_pred_classes = np.argmax(y_pred, axis=1)
 
-# Compute confusion matrix
 cm = confusion_matrix(y_test, y_pred_classes)
 
-# Plot confusion matrix
 plt.figure(figsize=(10, 8))
-sns.heatmap(cm, annot=True, fmt="g", cmap="Blues")
+sns.heatmap(
+    cm,
+    annot=True,
+    fmt="g",
+    cmap="Blues",
+    xticklabels=label_names,
+    yticklabels=label_names,
+)
 plt.xlabel("Predicted Label")
 plt.ylabel("True Label")
 plt.show()
